89b5dd370d88b3231fc8f936cdd7169aebf187ec
Milos Seskar <milos.seskar2@gmail.com>
2024-03-05 13:27:48 -0500

    Script for cleaning up ACS data

---
+++ acs_cleaning.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+income_filename = ''
+demog_filename = ''
+
+acs_income_df = pd.read_csv(income_filename, usecols=[])
+acs_demog_df = pd.read_csv(demog_filename, usecols=[])
+
